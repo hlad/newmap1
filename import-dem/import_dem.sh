162811d9355297bcb786922bc12838ec9d7ee155
hlad <hladma@gmail.com>
2024-08-13 12:51:23 +0200

    using ght instead of tif

--- import-dem/import_dem.sh
+++ import-dem/import_dem.sh
@@ -5,7 +5,7 @@ echo "# Importing contours #"
 echo "######################"
 echo ""
 first=1
-for dem_file in `ls "$DEM_DIR"/*.tif | sort -V`; do
+for dem_file in `ls "$DEM_DIR"/*.hgt | sort -V`; do
     echo "Importing $dem_file"
     if [ -n $first ]; then
         gdal_contour -f PostgreSQL -snodata -32768 -a ele -i 10 -lco OVERWRITE=yes $dem_file \
@@ -23,11 +23,11 @@ echo "# Importing hillshade #"
 echo "#######################"
 echo ""
 
-for dem_file in `ls "$DEM_DIR"/*.tif | sort -V`; do
+for dem_file in `ls "$DEM_DIR"/*.hgt | sort -V`; do
     dem_base=$(basename $dem_file)
     echo "$HILLSHADE_DIR/$dem_base"
     gdaldem hillshade -z 3 -alt 66 $dem_file "$HILLSHADE_DIR/$dem_base"
 done
 
 
-raster2pgsql -c -I -Y -t 256x256 $HILLSHADE_DIR/*.tif hillshade | PGPASSWORD=$POSTGRES_PASSWORD psql -U $POSTGRES_USER -d $POSTGRES_DB -h $POSTGRES_HOST -p $POSTGRES_PORT
+raster2pgsql -c -I -Y -t 256x256 $HILLSHADE_DIR/*.hgt hillshade | PGPASSWORD=$POSTGRES_PASSWORD psql -U $POSTGRES_USER -d $POSTGRES_DB -h $POSTGRES_HOST -p $POSTGRES_PORT
